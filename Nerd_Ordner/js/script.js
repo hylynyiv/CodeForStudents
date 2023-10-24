c80927738001ac4d7e7bb299f7205159afce9714
hylynyiv <info@hylynyiv.com>
2023-10-24 23:03:56 +0200

    First

---
+++ Nerd_Ordner/js/script.js
@@ -0,0 +1,20 @@
+// Variables
+var myVar = 2
+let user = "Alice"
+const myPets = ["bird", "cat", "dog"]
+ 
+let isActive = false
+
+// Built-in Console function
+console.log(myPets)
+
+// Functions
+function hello (name) {
+    console.log(name)
+}
+hello("Bob")
+hello(user);
+
+/// Simple DOM Manipulation
+document.body.innerHTML = user;
+// document.write(5+6)
