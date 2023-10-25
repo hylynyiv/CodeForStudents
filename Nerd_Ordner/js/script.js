7cda96ce0944f09d76d40bb604cc2be58b7669b7
hylynyiv <info@hylynyiv.com>
2023-10-25 14:45:43 +0200

    CreateElementExamples

--- Nerd_Ordner/js/script.js
+++ Nerd_Ordner/js/script.js
@@ -1,35 +1,76 @@
-// 1. Variables and Datatypes
-var myVar = 2
-let user = "Alice"
-let user2 = "Bob"
-const myPets = ["bird", "cat", "dog"]
-let isActive = false
+// // 1. Variables and Datatypes
+// var myVar = 2
+// let user = "Alice"
+// let user2 = "Bob"
 
-// 2. Built-in Console.log Function
-console.log(myPets)
+// let greetings = ["Hello", "Bonsoir", "Moin"]
 
-// 3. Functions
-function hello (name) {
-    console.log(name)
-}
-hello("Bob")
-hello(user);
+// greetings[0] = "Hi" 
+// let isActive = false
 
-/// 4. Simple DOM Manipulation
-document.body.innerHTML = user;
+// // 2. Built-in Console.log Function
+// // console.log(myPets)
+
+// // 3. Functions
+// function hello (name) {
+//     console.log(name)
+// }
+// hello("Bob")
+// hello(user);
+
+// 4. Simple DOM Manipulation
+// document.body.innerHTML = user;
 
 // 5. For-Loop
+// for (let i=0; i<=2; i++ ){
+//     console.log(greetings[i] + " " + myPets[i])
+// }
+
+// const myPets = ["bird", "cat", "dog"]
+
+// for (let pet of myPets){
+//     console.log(pet)
+// }
 
 // 6. DOM Manipulation
-// getElementById 
+// document.getElementById("main").innerHTML = "CATS are gods!!!!" 
 
-// querySelector
+let paragraph1 = document.querySelector("p")
+paragraph1.innerHTML = "Immer noch voll toll!"+"<br>"
 
-// createElement 
+// let myLink = document.createElement("a")
+// myLink.innerHTML = "Wetter" 
+// myLink.href = "http://wetter.com"
+// myLink.target = "_blank"
 
 // appendChild 
+// document.body.appendChild(myLink)
 
 // removeChild
+
+/// Exercise
+
+let myPets = ["bird", "fish", "dog"]
+let myLinks = ["https://en.wikipedia.org/wiki/Bird","https://en.wikipedia.org/wiki/Dog", "https://en.wikipedia.org/wiki/Cat"]
+
+for (let i=0; i<3; i++){
+    //neues Element pro Tier erzeugen
+    let animal = document.createElement("a")
+    animal.innerHTML = myPets[i]
+    animal.href = myLinks[i]
+    animal.target = "_blank"
+
+    // An existierendes Element dranhängen
+    paragraph1.appendChild(animal)
+
+    let btn = document.createElement("button")
+    paragraph1.appendChild(btn)
+
+    // Seitenumbruch pro Tier
+    let br = document.createElement("br")
+    paragraph1.appendChild(br)
+}
+
 
 
 // 7. Event Handlers
